59213149ff25f30c43a20797eea573917a7e6914
YY <yy@YYdeMacBook-Pro.local>
2021-01-26 11:29:11 -0500

    small update

--- images/Q4.py
+++ images/Q4.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 I =  io.imread('gigi.jpg')
+I[I<50] = 50
 I = I - 50
 I = img_as_float32(I)
-plt.imshow( I )
-plt.savefig('gigi_reduced.jpg')
+plt.imshow(I)
+plt.show()
+##plt.savefig('gigi_reduced.jpg')
